4901437be6dd406be0a8135b77c6f9e6d351bdb6
jkvn <ceepkev77@gmail.com>
2024-02-25 01:49:53 +0100

    Update build settings and dependencies
    The build plugin has been updated from 'java' to 'java-library', in addition to adding 'maven-publish' for publishing artifacts. Several dependencies have also been added, including one for project Lombok. Lastly, configuration for publishing to a Maven repository has been implemented.
    
    Co-authored-by: 83925560+swnck@users.noreply.github.com

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id("java-library")
+    id("maven-publish")
 }
 
 group = "at.jkvn.eventlib"
@@ -10,18 +11,38 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks {
-    jar {
-        manifest {
-            attributes["Main-Class"] = "at.jkvn.eventlib.EventLib"
-        }
-    }
+    implementation("org.reflections:reflections:0.9.12")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "nexus"
+            val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
+            url = if (isSnapshot)
+                uri("https://nexus.synclyn.com/repository/maven-snapshots/")
+            else
+                uri("https://nexus.synclyn.com/repository/maven-releases/")
+            credentials {
+                username = System.getenv("NEXUS_USERNAME")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
 }
