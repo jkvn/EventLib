d295eac8155b9ba76d0cb362487527f5a5fc2000
swnck <datapack3t.contact@gmail.com>
2024-02-25 16:02:11 +0100

    Implemented event priority

--- src/test/java/TestProject.java
+++ src/test/java/TestProject.java
@@ -1,25 +1,16 @@
 import at.jkvn.eventlib.EventLib;
-import at.jkvn.eventlib.Listener;
 import at.jkvn.eventlib.annotation.EventHandler;
 import at.jkvn.eventlib.registry.Configuration;
 import at.jkvn.eventlib.registry.ListenerRegistryType;
 import events.StartupEvent;
-import listener.StartupListener;
 
 public class TestProject {
     public static void main(String[] args) {
         EventLib.configure(Configuration.builder()
                 .allowedHosts(new String[]{})
-                .type(ListenerRegistryType.MANUAL)
+                .type(ListenerRegistryType.AUTOMATIC)
                 .build());
 
-        EventLib.registerListener(new StartupListener());
-        System.out.println("Hello, World!");
         EventLib.call(new StartupEvent());
-    }
-
-    @EventHandler
-    public void onStartup(StartupEvent event) {
-        System.out.println("Startup event called at ms");
     }
 }
